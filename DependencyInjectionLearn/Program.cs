8436c6ac6d97e69f556da71f34650eaa8e11ddf0
Artyom Sushenya <asuschenya@gmail.com>
2016-08-18 18:21:52 +0300

    Çàãîòîâêà

---
+++ DependencyInjectionLearn/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ninject.Modules;
+using Ninject;
+namespace DependencyInjectionLearn
+{
+    public interface IWeapon
+    {
+        void Kill();
+    }
+
+    public class Bazuka: IWeapon
+    {
+        public void Kill()
+        {
+            Console.WriteLine("BIG Badabum");
+        }
+    }
+
+    public class Sword : IWeapon
+    {
+        public void Kill()
+        {
+            Console.WriteLine("Chuk-chuck");
+        }
+    }
+
+    public class Warrior
+    {
+        readonly IWeapon Weapon;
+
+        public Warrior(IWeapon weapon)
+        {
+            this.Weapon = weapon;
+        }
+
+        public void Kill()
+        {
+            Weapon.Kill();
+        }
+    }
+
+    public class WeaponNinjectModule: NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IWeapon>().To<Sword>();
+        }
+    }
+
+    public class AnotherWarrior
+    {
+        [Inject]
+        public IWeapon Weapon { get; set; }
+        
+
+        public void Kill()
+        {
+            Weapon.Kill();
+        }
+    }
+    /// <summary>
+    /// теперь Schedule
+    /// </summary>
+    /// 
+    
+    class Program
+    {
+       // public static Ninject.IKernel AppKernel;
+        static void Main(string[] args)
+        {
+            Ninject.IKernel AppKernel = new StandardKernel(new WeaponNinjectModule());
+            var warrior = AppKernel.Get<AnotherWarrior>();
+            warrior.Kill();
+
+        }
+    }
+}
